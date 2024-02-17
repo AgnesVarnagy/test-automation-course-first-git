fcd1d6f601cd089b45793400c19cd20f415ad149
AgnesVarnagy <agnes.vrngy@gmail.com>
2024-02-17 17:15:38 +0100

    added this files to the master branch

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+
+        byte declaration = 3; // megadom a típusát és  a változó nevát
+        System.out.println(declaration);
+        byte initialization = 1; // ha megadom a változó típusát és nevét valamint egyből értéket is adok neki
+        System.out.println("ini");
+
+        byte number = 1;
+
+        number = 2;
+
+        int number1 = 1;
+        System.out.println(number);
+
+    }
+// jellel jelöljük a kommentetzett részeket
+ /*
+ Ezzel a jellel egy egész szakaszt tudunk
+ kommentezni. és így jóbb
+  */
+
+//TODO vmkit itt abba hagytam és innen folytatnám, ha csengettet a postás vagy vmi miatt abba hagytuk és folytatni kell
+
+
+
+}
+
+
+
